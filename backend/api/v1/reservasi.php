8779ec8d32b4fd62e00d34f1b9b2d1cd0195362e
keqi000 <144864650+keqi000@users.noreply.github.com>
2024-06-05 22:16:09 +0800

    Add files via upload

--- backend/api/v1/reservasi.php
+++ backend/api/v1/reservasi.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 
 $PROJECT_DIRECTORY = dirname(__FILE__, 4);
@@ -6,11 +7,9 @@ $PROJECT_DIRECTORY = dirname(__FILE__, 4);
 require_once($PROJECT_DIRECTORY . "/backend/connect.php");
 require_once($PROJECT_DIRECTORY . "/backend/handler/request.php");
 require_once($PROJECT_DIRECTORY . "/backend/handler/response.php");
-require_once($PROJECT_DIRECTORY . "/backend/api/v1/_lib/login.php");
+require_once($PROJECT_DIRECTORY . "/backend/api/v1/_lib/reservasi.php");
 
-
-$handler = new LoginHandler($_SERVER, $connection);
+$handler = new ReservasiHandler($_SERVER, $connection);
 $response = handle_request($handler);
-
 
 json_response($response);
