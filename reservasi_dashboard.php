960bc1b91b867c6c887d93e1ec809314d598bc1d
sihotang-yonathan1 <143815443+sihotang-yonathan1@users.noreply.github.com>
2024-06-07 17:25:22 +0800

    Change title

--- reservasi_dashboard.php
+++ reservasi_dashboard.php
@@ -30,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
 
 <head>
-    <title>Reservasi Meja Restoran WarongWarem</title>
+    <title>Ancol Reservasi Dashboard</title>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
