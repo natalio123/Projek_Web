19b110f7c384ebe55b2dd6f9a9df51f472514cb3
sihotang-yonathan1 <143815443+sihotang-yonathan1@users.noreply.github.com>
2024-06-07 17:23:27 +0800

    Change title

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 <html lang="en">
 
 <head>
-    <title>Restauran Login</title>
+    <title>Ancol Login</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="style.css" />
